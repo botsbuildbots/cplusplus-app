8f9d1ada8d09a5261bccaaa3ca3a986b8840c234
botsbuildbots <119365865+botsbuildbots@users.noreply.github.com>
2024-03-13 20:02:37 -0500

    Finished accessor definitions for Student class

--- student.cpp
+++ student.cpp
@@ -44,3 +44,41 @@ int* Student::GetDaysInCourse() {
 DegreeProgram Student::GetDegreeProgram() const {
   return this->degreeProgram;
 }
+
+// mutators
+void Student::SetStudentID(string ID) {
+  this->studentID = ID;
+}
+void Student::SetFirstName(string first) {
+  this->firstName = first;
+}
+void Student::SetLastName(string last) {
+  this->lastName = last;
+}
+void Student::SetEmail(string email) {
+  this->emailAddress = email;
+}
+void Student::SetAge(int age) {
+  this->studentAge = age;
+}
+void Student::SetDaysInCourse(int* days) {
+  this->daysInCourse[0] = days[0];
+  this->daysInCourse[1] = days[1];
+  this->daysInCourse[2] = days[2];
+}
+void Student::SetDegreeProgram(DegreeProgram program) {
+  this->degreeProgram = program;
+}
+
+// print() does not need arguments, it will just use class accessors    
+void Student::Print() {
+  // this->GetDegreeProgram() should resolve to a value (0,1,2) that can be used to match one of these strings
+  const char* EnumDeterminate[] = { "SECURITY", "NETWORK", "SOFTWARE" };
+
+  // A1 [tab] First Name: John [tab] Last Name: Smith [tab] Age: 20 [tab]daysInCourse: {35, 40, 55} Degree Program: Security
+  // this->GetDaysInCourse()[index] should be the same as this.daysInCourse[index], hopefully
+  cout << this->GetStudentID() << "\t First Name: " << this->GetFirstName() << "\t Last Name: "                  << 
+          this->GetLastName() << "\t Age: " << this->GetAge() << "\t daysInCourse: {"                            << 
+          this->GetDaysInCourse()[0] << ", " << this->GetDaysInCourse()[1] << ", " << this->GetDaysInCourse()[2] <<
+          "} Degree Program: " << EnumDeterminate[this->GetDegreeProgram()]                                      << endl;
+}
