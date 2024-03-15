5a2b7eb13d1926a2bf73a67cd353e82235fd022f
botsbuildbots <119365865+botsbuildbots@users.noreply.github.com>
2024-03-14 20:07:59 -0500

    Added required function definitions to roster.h

--- roster.h
+++ roster.h
@@ -7,12 +7,21 @@ using namespace std;
 
 class Roster {
   private:
-    // declares an array of pointers with an undefined size, this will be populated with pointers to Student class instances
-    Student** classRosterArray = { nullptr };
+    // declares a pointer that will be used to build array of student objects
+    Student *classRosterArray;
   public:
-    // constructor takes in the number of students to be added to the array
+    // constructor takes in the number of students to be added to the array, should create array for classRosterArray with that count
+    // classRosterArray = new Student[studentCount];
+    // alternatively just delcare classRosterArray with five students and then have add() create new Student and place at each index then ++index
     Roster(int studentCount);
     ~Roster();
+    void Add(string studentID, string firstName, string lastName, string emailAddress, int age, 
+      int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram);
+    void Remove(string studentID);
+    void PrintAll();
+    void PrintAverageDaysInCourse(string studentID);
+    void PrintInvalidEmails();
+    void PrintByDegreeProgram(DegreeProgram degreeProgram);
 };
 
 #endif
